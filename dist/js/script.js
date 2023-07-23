e4ef7ad7b165fa98095c8d911f629450738919fc
Cranival <email>
2023-07-23 14:32:38 +0700

    Menambahkan fitu contact dan animasi

--- dist/js/script.js
+++ dist/js/script.js
@@ -54,4 +54,31 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 }
 
 // input contact
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwk4_bFiBj6V9fX31aFkccSN8oFUOKF0EhqRNTYCLkhfqE96-4Y-VtrTiN-9JXa8XMS/exec'
+const form = document.forms['form-contact']
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+    //ketika tombil kirim diklik
+    //tampilkan tombil loading, hilangkan tombil kirim
+    btnLoading.classList.toggle("hidden");
+    btnKirim.classList.toggle("hidden");
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      //tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("hidden");
+      btnKirim.classList.toggle("hidden");
+      //tampilkan alert
+      myAlert.classList.toggle("hidden");
+      //reset formnya
+      form.reset();
+      console.log('Success!', response);
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+// akhir input contact
+
 
